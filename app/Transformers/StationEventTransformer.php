19c93a26390cce38444db9ea69ac6848a551bd2e
Clemente Estrada <mirdrack@gmail.com>
2016-04-02 22:04:09 -0600

    Mods for report filters

--- app/Transformers/StationEventTransformer.php
+++ app/Transformers/StationEventTransformer.php
@@ -9,22 +9,22 @@ class StationEventTransformer extends Transformer
     public function transform($object)
     {
         return [
-            'id' => $object->id,
-            'user_id' => $object->user_id,
-            'station_id' => $object->station_id,
-            'event_type_id' => $object->event_type_id,
+            'id' => (int) $object->id,
+            'user_id' => (int) $object->user_id,
+            'station_id' => (int) $object->station_id,
+            'event_type_id' => (int) $object->event_type_id,
             'ip_address' => $object->ip_address,
             'created_at' => date('d-m-Y @ H:i:s', strtotime($object->created_at)),
             'user' => [
-                'id' => $object->user->id,
+                'id' => (int) $object->user->id,
                 'name' => $object->user->name,
             ],
             'station' => [
-                'id' => $object->station->id,
+                'id' => (int) $object->station->id,
                 'name' => $object->station->name,
             ],
             'event_type' => [
-                'id' => $object->event_type->id,
+                'id' => (int) $object->event_type->id,
                 'label' => $object->event_type->label,
             ],
         ];
